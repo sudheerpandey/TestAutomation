285e926d7886ef73ccc3e4195cb7e4ad0e2e0f62
Sudheer <pandey.sudheer1991@gmail.com>
2019-01-05 12:56:53 +0530

    home page added

---
+++ src/test/java/com/qa/guru/Testcases/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.qa.guru.Testcases;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void loginTest()
+	{
+		System.out.println("Test ");
+	}
+
+}
